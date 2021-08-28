3a842e1b252e5360e9d78cf0ed6c7a7bbd8a6ae7
eliza <twew02@gmail.com>
2021-08-27 21:59:15 -0400

    initial

---
+++ server/db/schema/05_groups.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS groups CASCADE;
+
+CREATE TABLE groups (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  category_id INT NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+  description TEXT NOT NULL,
+  photo VARCHAR(255)
+  
+);
