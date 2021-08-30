9f3b0aa12e5873ab06ca89da08154b7b4c58f6b6
eliza <twew02@gmail.com>
2021-08-30 15:14:45 -0400

    minor change

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,10 +5,7 @@ module.exports = (db) => {
   router.get("/:id", (req, res) => {
     req.session.user_id = req.params.id;
     db.query(`select first_name from users where users.id = ${req.session.id};`)
-      .then(data => {
-        userEmail = data.rows[0].email;
-
-      });
+     
     res.redirect('/');
   });
   return router;
