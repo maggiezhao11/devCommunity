607fff04931edb8e1692f20e1f03614f1b5bfe02
eliza <twew02@gmail.com>
2021-08-30 09:58:13 -0400

    add routes events

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const router  = express.Router();
 
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const postId = req.params.id;
+    db.query(`SELECT * FROM events ORDER BY id desc;`)
+    .then(data => {
+      res.json(data.rows);
+    });
+  });
+
+  //filter events by category
+  router.get("/filter", (req, res) => {
+    db.query(`SELECT * FROM events JOIN categories ON categories.id = events.category_id  WHERE topic = $1;`, [req.body.topic])
+    .then(data => {
+      res.json(data.rows);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  })
+  return router;
+}
+
