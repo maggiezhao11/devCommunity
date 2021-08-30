154e3d08e33ea9777c2e549376f67c2c90cfd112
eliza <twew02@gmail.com>
2021-08-30 15:00:35 -0400

    resolved issues with pg_config error, install cookie session and update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  secret: 'secret'
+}))
 //middleware
 app.use(morgan("dev"));
 app.use(express.json());
@@ -14,18 +18,21 @@ app.use(cors());
 const posts = require('./routes/posts');
 const users = require('./routes/users')
 const groups = require('./routes/groups');
-const events = require('./routes/events')
+const events = require('./routes/events');
+const login = require('./routes/login');
 
 app.use('/posts', posts(db));
 app.use('/users', users(db));
 app.use('/groups', groups(db));
 app.use('/events', events(db));
+app.use('/login', login(db));
 
 app.get("/", (req, res) => {
   
     res.send("ok");
     //res.json(data);
   });
+
 
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
