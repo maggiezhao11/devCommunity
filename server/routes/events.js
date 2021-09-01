5b49c23d5c9b0b75c759a9332dfe9893ba59f0f2
eliza <twew02@gmail.com>
2021-09-01 17:22:40 -0400

    modified routes for upcoming events

--- server/routes/events.js
+++ server/routes/events.js
@@ -10,6 +10,25 @@ module.exports = (db) => {
     });
   });
 
+  //get joined event and display in upcoming events
+  router.get("/upcoming", (req, res) => {
+    const user_id = req.session.user_id
+    db.query(`SELECT * FROM events WHERE user_id = $1 ORDER BY date asc;`, [user_id])
+    .then(data => {
+      res.json(data.rows);
+    });
+  })
+//join an event
+  router.post("/join", (req, res) => {
+    const user_id = req.session.user_id
+    const event_id = req.body.id
+    
+    return db.query(`UPDATE events SET user_id = $1 WHERE id = $2 returning *;`, [user_id, event_id])
+    .then(data => {
+      res.json(data.rows);
+    })
+  })
+
   //filter events by category
   router.post("/filter", (req, res) => {
     db.query(`SELECT events.*, categories.topic FROM events JOIN categories ON categories.id = events.category_id  WHERE topic = $1;`, [req.body.topic])
