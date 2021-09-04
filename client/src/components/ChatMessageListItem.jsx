da36df3f8baceac2ad4da8894a23c8882bd374c7
eliza <twew02@gmail.com>
2021-09-04 14:48:23 -0400

    changed color of chat window

--- client/src/components/ChatMessageListItem.jsx
+++ client/src/components/ChatMessageListItem.jsx
@@ -5,12 +5,14 @@ export default function ChatMessageListItem(props) {
   const { message, time, author} = props
   return (
     <div>
+      
+      <div className="message-meta">
+        <p id="author">{author} </p>
+        <p id="time">{time}</p>
+        
+      </div>
       <div className="message-content">
         <p>{message}</p>
-      </div>
-      <div className="message-meta">
-        <p id="time">{time}</p>
-        <p id="author">{author}</p>
       </div>
     </div>
   )
